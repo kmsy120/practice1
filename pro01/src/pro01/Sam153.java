a769068e45064e132a27ca336c4e4c4287272e7a
kmsy120 <kmsy120@naver.com>
2022-04-29 11:50:07 +0900

    0429

---
+++ pro01/src/pro01/Sam153.java
@@ -0,0 +1,44 @@
+package pro01;
+
+import java.util.*;
+import java.io.*;
+public class Sam153 {
+ public static void main(String[] args) {
+	 
+
+	 Scanner sc = new Scanner(System.in);
+
+	 String[] e = sc.nextLine().split("");
+	 StringBuilder t = new StringBuilder();
+
+
+	 switch(Integer.parseInt(e[0])) {
+	 case 0 : break;
+	 case 1 : t.append("1"); break;
+	 case 2 : t.append("10"); break;
+	 case 3 : t.append("11"); break;
+	 case 4 : t.append("100"); break;
+	 case 5 : t.append("101"); break;
+	 case 6 : t.append("110"); break;
+	 case 7 : t.append("111"); break;
+	 default : break;      
+	  }
+	 for(int i=1;i<e.length;i++) {
+	 	
+	      switch(Integer.parseInt(e[i])) {
+	       case 0 : t.append("000"); break;
+	       case 1 : t.append("001"); break;
+	       case 2 : t.append("010"); break;
+	       case 3 : t.append("011"); break;
+	       case 4 : t.append("100"); break;
+	       case 5 : t.append("101"); break;
+	       case 6 : t.append("110"); break;
+	       case 7 : t.append("111"); break;
+	       default : break;
+	       }
+	      }
+	   long r = Long.parseLong(t.toString());
+	  System.out.print(r);
+}
+}
+
